e4d0de3972e9eb2708ad74b280f96df8edf091f3
ReeceJones <reece_jones@icloud.com>
2019-03-09 14:13:59 -0800

    re-added kbbuild

--- source/backend/parts/kbbuild.d
+++ source/backend/parts/kbbuild.d
@@ -5,5 +5,10 @@ import backend.structops;
 
 struct KeyboardBuild
 {
-    
+    KeyboardCase kbcase;                /// The case that they are using
+    KeyboardPlate kbplate;              /// The plate that they are going to use
+    KeyboardPCB kbpcb;                  /// The PCB that they are going to use
+    KeyBoardSwitch[] kbswitches;        /// The switches that they are going to use. (They can use multiple different switches)
+    KeyboardKeycap[] kbkeycaps;         /// The keycaps that they re going to use. (The can use multiple different keysets)
+    KeyboardAccessory[] kbaccessories;  /// The various accessories that they want to get with their keyboard.
 }
